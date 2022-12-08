dd769f2392deb85cab949069b4664f801a638a80
phitrongtuanthanh <phitrongtuanthanh@gmail.com>
2022-12-08 00:43:17 +0000

    add essentials

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,23 @@
+var questions = [
+    {
+        questionTitle: '4 is a ... value',
+        choices: ['boolean', 'string', 'null', 'num'],
+        answer: 'num',
+
+    },
+    {
+        questionTitle: '5 is a ... value',
+        Choices: ['boolean', 'string', 'null', 'undefined'],
+        answer: 'num',
+    },
+    {
+        questionTitle: '6 is a ... value',
+        Choices: ['boolean', 'string', 'null', 'undefined'],
+        answer: 'num',
+    },
+    {
+        questionTitle: '7 is a ... value',
+        Choices: ['boolean', 'string', 'null', 'undefined'],
+        answer: 'num',
+    }
+];

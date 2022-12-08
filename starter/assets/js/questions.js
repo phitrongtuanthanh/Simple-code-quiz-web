d664358c34170f79a1420b9b53b680547dd21be8
phitrongtuanthanh <phitrongtuanthanh@gmail.com>
2022-12-08 15:22:36 +0000

    add-essentials

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,23 +1,23 @@
-var questions = [
+var questionsList = [
     {
         questionTitle: '4 is a ... value',
         choices: ['boolean', 'string', 'null', 'num'],
-        answer: 'num',
+        answer: 'choice-one',
 
     },
     {
         questionTitle: '5 is a ... value',
-        Choices: ['boolean', 'string', 'null', 'undefined'],
-        answer: 'num',
+        choices: ['boolean', 'string', 'null', 'undefined'],
+        answer: 'choice-one',
     },
     {
         questionTitle: '6 is a ... value',
-        Choices: ['boolean', 'string', 'null', 'undefined'],
-        answer: 'num',
+        choices: ['boolean', 'string', 'null', 'undefined'],
+        answer: 'choice-one',
     },
     {
         questionTitle: '7 is a ... value',
-        Choices: ['boolean', 'string', 'null', 'undefined'],
-        answer: 'num',
+        choices: ['boolean', 'string', 'null', 'undefined'],
+        answer: 'choice-one',
     }
 ];
